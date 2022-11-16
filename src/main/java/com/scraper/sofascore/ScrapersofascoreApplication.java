e0409caed21a78beaca88e6a5bf8397205b8d287
joel270687 <joel270687@gmail.com>
2022-11-16 18:30:48 -0300

    Initial commit

---
+++ src/main/java/com/scraper/sofascore/ScrapersofascoreApplication.java
@@ -0,0 +1,13 @@
+package com.scraper.sofascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapersofascoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapersofascoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scraper/sofascore/ScrapersofascoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scraper.sofascore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapersofascoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
